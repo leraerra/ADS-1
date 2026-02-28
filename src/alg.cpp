8252f06b750f9a1a0521a56ec682b2f06b6de815
leraerra <valeriaberbasova@yandex.ru>
2026-03-01 00:41:07 +0300

    Бербасова Валерия ист4-1

--- src/alg.cpp
+++ src/alg.cpp
@@ -6,7 +6,7 @@
 bool checkPrime(uint64_t value) {
   if (value <= 1) return false;
   if (value == 2) return true;
-  if (value % 2 == 0) return false;  
+  if (value % 2 == 0) return false;
   for (uint64_t i = 3; i * i <= value; i += 2) {
     if (value % i == 0) {
             return false;
@@ -16,7 +16,7 @@ bool checkPrime(uint64_t value) {
 }
 
 uint64_t nPrime(uint64_t n) {
-  if (n == 0) return 0; 
+  if (n == 0) return 0;
   uint64_t count = 0;
   uint64_t number = 1;
   while (count < n) {
@@ -39,7 +39,7 @@ uint64_t nextPrime(uint64_t value) {
 }
 
 uint64_t sumPrime(uint64_t hbound) {
-  if (hbound <= 2) return 0;   
+  if (hbound <= 2) return 0;  
   uint64_t sum = 0;
   for (uint64_t i = 2; i < hbound; i++) {
     if (checkPrime(i)) {
