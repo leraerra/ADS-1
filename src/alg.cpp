29910c07638d76097bc0574acb8eb8c312267b46
leraerra <valeriaberbasova@yandex.ru>
2026-03-01 00:10:01 +0300

    Бербасова Валерия 4-1

--- src/alg.cpp
+++ src/alg.cpp
@@ -6,22 +6,19 @@
 bool checkPrime(uint64_t value) {
   if (value <= 1) return false;
   if (value == 2) return true;
-  if (value % 2 == 0) return false;
-    
-    for (uint64_t i = 3; i * i <= value; i += 2) {
+  if (value % 2 == 0) return false;  
+  for (uint64_t i = 3; i * i <= value; i += 2) {
         if (value % i == 0) {
             return false;
         }
-    }
+  }
   return true;
 }
 
 uint64_t nPrime(uint64_t n) {
-  if (n == 0) return 0;
-    
+  if (n == 0) return 0; 
   uint64_t count = 0;
   uint64_t number = 1;
-    
   while (count < n) {
         number++;
         if (checkPrime(number)) {
@@ -33,7 +30,6 @@ uint64_t nPrime(uint64_t n) {
 
 uint64_t nextPrime(uint64_t value) {
   uint64_t number = value + 1;
-    
   while (true) {
         if (checkPrime(number)) {
             return number;
@@ -43,8 +39,7 @@ uint64_t nextPrime(uint64_t value) {
 }
 
 uint64_t sumPrime(uint64_t hbound) {
-  if (hbound <= 2) return 0;
-    
+  if (hbound <= 2) return 0;   
   uint64_t sum = 0;
   for (uint64_t i = 2; i < hbound; i++) {
         if (checkPrime(i)) {
@@ -56,7 +51,6 @@ uint64_t sumPrime(uint64_t hbound) {
 
 uint64_t twinPrimes(uint64_t lbound, uint64_t hbound) {
   uint64_t count = 0;
-    
   for (uint64_t i = lbound; i <= hbound - 3; i++) {
         if (i + 2 < hbound) {
             if (checkPrime(i) && checkPrime(i + 2)) {
